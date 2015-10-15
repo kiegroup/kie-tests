6b4295d23ba7f9148306a7f57d6d75d34cca2cdc
Ivo Bek <ibek@redhat.com>
2015-10-15 15:08:20 +0200

    performance-tests: separated LHumanTaskProcess scenario with listeners and without listeners closes #12
    (cherry picked from commit a9db00f3729dcca83a51459b0c011459fd28fd5f)

--- performance-tests/jbpm-performance-tests/src/main/java/org/kie/perf/jbpm/JBPMController.java
+++ performance-tests/jbpm-performance-tests/src/main/java/org/kie/perf/jbpm/JBPMController.java
@@ -83,6 +83,7 @@ public class JBPMController {
                 ex.printStackTrace();
             }
         }
+        instance.clearCustomEntries();
         return instance;
     }
 
--- performance-tests/jbpm-performance-tests/src/main/java/org/kie/perf/scenario/load/LHumanTaskProcessWithListeners.java
+++ performance-tests/jbpm-performance-tests/src/main/java/org/kie/perf/scenario/load/LHumanTaskProcessWithListeners.java
@@ -2,9 +2,13 @@ package org.kie.perf.scenario.load;
 
 import java.util.List;
 
+import org.jbpm.services.task.events.DefaultTaskEventListener;
+import org.kie.api.event.process.DefaultProcessEventListener;
+import org.kie.api.event.process.ProcessCompletedEvent;
 import org.kie.api.runtime.KieSession;
 import org.kie.api.runtime.manager.RuntimeEngine;
 import org.kie.api.runtime.process.ProcessInstance;
+import org.kie.api.task.TaskEvent;
 import org.kie.api.task.TaskService;
 import org.kie.perf.SharedMetricRegistry;
 import org.kie.perf.jbpm.JBPMController;
@@ -12,29 +16,55 @@ import org.kie.perf.jbpm.constant.ProcessStorage;
 import org.kie.perf.jbpm.constant.UserStorage;
 import org.kie.perf.scenario.IPerfTest;
 
+import com.codahale.metrics.Meter;
 import com.codahale.metrics.MetricRegistry;
 import com.codahale.metrics.Timer;
 
-public class LHumanTaskProcess implements IPerfTest {
+public class LHumanTaskProcessWithListeners implements IPerfTest {
 
     private JBPMController jc;
 
     private Timer startProcess;
     private Timer startTaskDuration;
     private Timer completeTaskDuration;
+    private Meter completedProcess;
+
+    private Meter taskStarted;
+    private Meter taskCompleted;
 
     @Override
     public void init() {
         jc = JBPMController.getInstance();
+        jc.setProcessEventListener(new DefaultProcessEventListener() {
+            @Override
+            public void afterProcessCompleted(ProcessCompletedEvent event) {
+                completedProcess.mark();
+            }
+        });
+        jc.setTaskEventListener(new DefaultTaskEventListener() {
+        	@Override
+        	public void afterTaskStartedEvent(TaskEvent event) {
+        		taskStarted.mark();
+        	}
+        	
+        	@Override
+        	public void afterTaskCompletedEvent(TaskEvent event) {
+        		taskCompleted.mark();
+        	}
+        });
+
         jc.createRuntimeManager(ProcessStorage.HumanTask.getPath());
     }
 
     @Override
     public void initMetrics() {
         MetricRegistry metrics = SharedMetricRegistry.getInstance();
-        startProcess = metrics.timer(MetricRegistry.name(LHumanTaskProcess.class, "scenario.process.start.duration"));
-        startTaskDuration = metrics.timer(MetricRegistry.name(LHumanTaskProcess.class, "scenario.task.start.duration"));
-        completeTaskDuration = metrics.timer(MetricRegistry.name(LHumanTaskProcess.class, "scenario.task.complete.duration"));
+        startProcess = metrics.timer(MetricRegistry.name(LHumanTaskProcessWithListeners.class, "scenario.process.start.duration"));
+        startTaskDuration = metrics.timer(MetricRegistry.name(LHumanTaskProcessWithListeners.class, "scenario.task.start.duration"));
+        completeTaskDuration = metrics.timer(MetricRegistry.name(LHumanTaskProcessWithListeners.class, "scenario.task.complete.duration"));
+        completedProcess = metrics.meter(MetricRegistry.name(LHumanTaskProcessWithListeners.class, "scenario.process.completed"));
+        taskStarted = metrics.meter(MetricRegistry.name(L1000HumanTasksStart.class, "scenario.task.started"));
+        taskCompleted = metrics.meter(MetricRegistry.name(L1000HumanTasksComplete.class, "scenario.task.completed"));
     }
 
     @Override
