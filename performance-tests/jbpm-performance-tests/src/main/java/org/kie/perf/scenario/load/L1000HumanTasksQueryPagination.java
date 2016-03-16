bd0b5b25cfc4515a0df90662c74f88f37ba7bb48
Ivo Bek <ibek@redhat.com>
2016-03-16 14:48:39 +0100

    jbpm-perf: updated L1000HumanTasksQueryPagination to use new task summary query

--- performance-tests/jbpm-performance-tests/src/main/java/org/kie/perf/scenario/load/L1000HumanTasksQueryPagination.java
+++ performance-tests/jbpm-performance-tests/src/main/java/org/kie/perf/scenario/load/L1000HumanTasksQueryPagination.java
@@ -3,9 +3,13 @@ package org.kie.perf.scenario.load;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.jbpm.kie.services.impl.RuntimeDataServiceImpl;
+import org.jbpm.services.api.RuntimeDataService;
+import org.jbpm.services.task.audit.TaskAuditServiceFactory;
+import org.jbpm.shared.services.impl.TransactionalCommandService;
 import org.kie.api.task.TaskService;
 import org.kie.api.task.model.TaskSummary;
-import org.kie.internal.task.api.InternalTaskService;
+import org.kie.internal.identity.IdentityProvider;
 import org.kie.perf.SharedMetricRegistry;
 import org.kie.perf.annotation.KPKLimit;
 import org.kie.perf.jbpm.JBPMController;
@@ -21,6 +25,7 @@ public class L1000HumanTasksQueryPagination implements IPerfTest {
 
     private JBPMController jc;
 
+    private RuntimeDataService runtimeDataService;
     private TaskService taskService;
 
     private List<TaskSummary> tasks = new ArrayList<TaskSummary>();
@@ -31,6 +36,12 @@ public class L1000HumanTasksQueryPagination implements IPerfTest {
         jc.createRuntimeManager();
 
         taskService = jc.getRuntimeEngine().getTaskService();
+        runtimeDataService = new RuntimeDataServiceImpl();
+        ((RuntimeDataServiceImpl) runtimeDataService).setCommandService(new TransactionalCommandService(jc.getEmf()));
+        ((RuntimeDataServiceImpl) runtimeDataService).setIdentityProvider(new TestIdentityProvider());
+        ((RuntimeDataServiceImpl) runtimeDataService).setTaskService(taskService);
+        ((RuntimeDataServiceImpl) runtimeDataService).setTaskAuditService(TaskAuditServiceFactory
+                .newTaskAuditServiceConfigurator().setTaskService(taskService).getTaskAuditService());
 
         PrepareEngine.createNewTasks(false, 1000, taskService);
     }
@@ -38,22 +49,47 @@ public class L1000HumanTasksQueryPagination implements IPerfTest {
     @Override
     public void initMetrics() {
         MetricRegistry metrics = SharedMetricRegistry.getInstance();
-        metrics.register(MetricRegistry.name(L1000HumanTasksQueryPagination.class, "scenario.tasks.query.page.size"), new Gauge<Integer>() {
-            @Override
-            public Integer getValue() {
-                return tasks.size();
-            }
-        });
+        metrics.register(MetricRegistry.name(L1000HumanTasksQueryPagination.class, "scenario.tasks.query.page.size"),
+                new Gauge<Integer>() {
+                    @Override
+                    public Integer getValue() {
+                        return tasks.size();
+                    }
+                });
     }
 
     @Override
     public void execute() {
-        tasks = ((InternalTaskService) taskService).taskQuery(UserStorage.PerfUser.getUserId()).intersect().maxResults(100).build().getResultList();
+        String userId = UserStorage.PerfUser.getUserId();
+        tasks = runtimeDataService.taskSummaryQuery(userId).maxResults(100).build().getResultList();
     }
 
     @Override
     public void close() {
         jc.tearDown();
+    }
+
+    public static class TestIdentityProvider implements IdentityProvider {
+
+        private List<String> roles = new ArrayList<String>();
+
+        public String getName() {
+            return UserStorage.PerfUser.getUserId();
+        }
+
+        public List<String> getRoles() {
+            return roles;
+        }
+
+        @Override
+        public boolean hasRole(String role) {
+            return roles.contains(role);
+        }
+
+        // just for testing
+        public void setRoles(List<String> roles) {
+            this.roles = roles;
+        }
     }
 
 }
